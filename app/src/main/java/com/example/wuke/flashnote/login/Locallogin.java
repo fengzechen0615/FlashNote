5c3f117c0e258772f04e869e4db28c38a127cc1c
francisfeng <13903131445@163.com>
2018-04-12 13:27:24 +0800

    杀了大哥祭天！修复了众多bug，添加了一些新功能

---
+++ app/src/main/java/com/example/wuke/flashnote/login/Locallogin.java
@@ -0,0 +1,92 @@
+package com.example.wuke.flashnote.login;
+
+import android.os.Environment;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Created by kumbaya on 2018/4/9.
+ */
+
+
+public class Locallogin {
+    public static void save(String a, String p){
+        try {
+            File account = Environment.getDataDirectory();
+            File newFile = new File(account, "/data/com.example.wuke.flashnote/account.txt");
+            FileWriter fw = new FileWriter(newFile, true);
+            /*if(check() == true){
+                String[] x = new String[2];
+                FileReader fr = new FileReader(newFile);
+                BufferedReader br = new BufferedReader(fr);
+                x[0] = br.readLine();
+                if(x[0]!=null){
+                    x[1] = br.readLine();
+                    if(x[0] == a && x[1] == p){
+                        fw.close();
+                    }
+                    else {
+                        newFile.delete();
+                        fw.write(a + "\n");
+                        fw.write(p + "\n");
+                        fw.close();
+                    }
+                }
+            }
+            else{
+                fw.write(a + "\n");
+                fw.write(p + "\n");
+
+                fw.close();
+            }*/
+            fw.write(a + "\n");
+            fw.write(p + "\n");
+            //Log.d("a", "yes");
+            fw.close();
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static String[] getaccount(){
+        String[] x = new String[2];
+        File account = Environment.getDataDirectory();
+        File newFile = new File(account, "/data/com.example.wuke.flashnote/account.txt");
+        try {
+            FileReader fr = new FileReader(newFile);
+            BufferedReader br = new BufferedReader(fr);
+            x[0] = br.readLine();
+            x[1] = br.readLine();
+            return x;
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static boolean check(){
+        File account = Environment.getDataDirectory();
+        File newFile = new File(account, "/data/com.example.wuke.flashnote/account.txt");
+        if(newFile.exists()){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public static void delete(){
+        File account = Environment.getDataDirectory();
+        File newFile = new File(account, "/data/com.example.wuke.flashnote/account.txt");
+        if(check()==true){
+            newFile.delete();
+        }
+        else {
+            return;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/wuke/flashnote/login/Login.java
@@ -0,0 +1,144 @@
+package com.example.wuke.flashnote.login;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.wuke.flashnote.MainActivity;
+import com.example.wuke.flashnote.R;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class Login extends AppCompatActivity {
+
+    private String url = "http://39.106.205.176:8080/artifacts/Servlet";
+    private EditText account;
+    private EditText password;
+    private TextView result;
+    private Button login;
+    private Button sign;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        account = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        result = (TextView) findViewById(R.id.result);
+
+
+        Locallogin l = new Locallogin();
+        if(l.check() == true) {
+            String[] x = l.getaccount();
+            account.setText(x[0]);
+            password.setText(x[1]);
+            access(x[0], x[1], "login");
+        }
+        
+        login = (Button) findViewById(R.id.login_button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(account.getText().toString() != null &&
+                        password.getText().toString() != null) {
+                    String a = account.getText().toString();
+                    String p = password.getText().toString();
+                    access(a, p, "login");
+
+                }
+            }
+        });
+
+        sign = (Button) findViewById(R.id.sign_button);
+        sign.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                if(account.getText().toString() != null &&
+                        password.getText().toString() != null) {
+                    String a = account.getText().toString();
+                    String p = password.getText().toString();
+                    access(a, p, "sign");
+                }
+            }
+        });
+
+        }
+
+    private void access(String a, String p, String l){
+        String s = url+"?account="+ a + "&password=" + p +"&login="+l;
+        new MyAsyncTask(result).execute(s);
+    }
+
+    public class MyAsyncTask extends AsyncTask<String ,Integer, String > {
+        private TextView tv;
+
+        Log log;
+
+        public MyAsyncTask(TextView v){
+            tv = v;
+        }
+
+        protected void onPreExecute(){
+            log.w("1", "task onPreExecute()");
+        }
+
+        protected String doInBackground (String... params){
+            log.w("1", "task DoInBackStage()");
+
+            HttpURLConnection connection = null;
+            StringBuilder response = new StringBuilder();
+
+            try {
+                System.setProperty("https.protocols", "TLSv1.2,TLSv1.1,SSLv3");
+                URL url = new URL(params[0]);
+
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setConnectTimeout(100000);
+                connection.setReadTimeout(100000);
+
+                InputStream input = connection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                String line;
+                while ((line = reader.readLine()) != null){
+                    response.append(line);
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return response.toString();
+        }
+
+        protected void onPostExecute(String s){
+            if(s.contains("resCode=201")){
+                Locallogin l = new Locallogin();
+                String a = account.getText().toString();
+                String p = password.getText().toString();
+                l.save(a,p);
+                Intent intent= new Intent (Login.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+            else{
+                tv.setText(s);
+            }
+        }
+    }
+}
