4777bccc8e23c26f024a4cbf4c4d715fa1950b3f
francisfeng <13903131445@163.com>
2018-05-19 22:31:40 +0800

    命令行UI

--- app/src/main/java/com/example/wuke/flashnote/setting/About.java
+++ app/src/main/java/com/example/wuke/flashnote/setting/About.java
@@ -2,14 +2,27 @@ package com.example.wuke.flashnote.setting;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.wuke.flashnote.R;
 
 public class About extends AppCompatActivity {
 
+    private TextView done;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        done = (TextView) findViewById(R.id.about_done);
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/wuke/flashnote/setting/Command.java
+++ app/src/main/java/com/example/wuke/flashnote/setting/Command.java
@@ -2,14 +2,26 @@ package com.example.wuke.flashnote.setting;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.wuke.flashnote.R;
 
 public class Command extends AppCompatActivity {
 
+    private TextView done;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_command);
+
+        done = (TextView) findViewById(R.id.command_done);
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
